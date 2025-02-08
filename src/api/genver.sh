92cdd91337d2768e71a4f6846c5ea6c058ef2463
codinuum <codinuum@me.com>
2025-02-08 16:54:29 +0900

    fix

--- src/api/genver.sh
+++ src/api/genver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rev=$(hg identify -n || git log --format=format:%h HEAD^! || echo unknown)
+rev=$(git log --format=format:%h HEAD^! || echo unknown)
 year=$(date "+%Y")
 echo "let version=\"revision ${rev}\"" > version.ml
 echo "let copyright=\"Copyright 2010-${year} Codinuum Software Lab <codinuum@me.com>\"" >> version.ml
