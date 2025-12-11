cb45353b61c2a32c7fe4f801160e9e62f6fa31bb
codinuum <codinuum@me.com>
2025-12-11 10:51:55 +0900

    fix (logically partial revert) for the added testcase

--- src/core/core/postprocessing.ml
+++ src/core/core/postprocessing.ml
@@ -5734,7 +5734,12 @@ end;
                | None, Some _ -> padj2 = Some true
                | _ when begin
                    not nd1#data#is_named_orig && not nd2#data#is_named_orig &&
-                   cenv#get_similarity_score (nmapping#inv_find nd2) nd2 < cenv#get_similarity_score nd1 nd2
+                   let sim_old = cenv#get_similarity_score (nmapping#inv_find nd2) nd2 in
+                   let sim_new = cenv#get_similarity_score nd1 nd2 in
+                   sim_new > 0.0 &&
+                   let sim_ratio = sim_old /. sim_new in
+                   [%debug_log "sim_old=%f sim_new=%f sim_ratio=%f" sim_old sim_new sim_ratio];
+                   sim_ratio < 1.5
                end ->
                  padj2 = Some true
                (*| _ when not nd1#data#has_non_trivial_value && not nd2#data#has_non_trivial_value ->
@@ -5748,7 +5753,12 @@ end;
                | Some _, None -> padj1 = Some true
                | _ when begin
                    not nd1#data#is_named_orig && not nd2#data#is_named_orig &&
-                   cenv#get_similarity_score nd1 (nmapping#find nd1) < cenv#get_similarity_score nd1 nd2
+                   let sim_old = cenv#get_similarity_score nd1 (nmapping#find nd1) in
+                   let sim_new = cenv#get_similarity_score nd1 nd2 in
+                   sim_new > 0.0 &&
+                   let sim_ratio = sim_old /. sim_new in
+                   [%debug_log "sim_old=%f sim_new=%f sim_ratio=%f" sim_old sim_new sim_ratio];
+                   sim_ratio < 1.5
                end ->
                  padj1 = Some true
                (*| _ when not nd1#data#has_non_trivial_value && not nd2#data#has_non_trivial_value ->
